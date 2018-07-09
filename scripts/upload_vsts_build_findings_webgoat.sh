5c3826cf27a5dcbab1de0cf2f6f5c7c67cd26d69
Timo Pawelka <pawelka@cqse.eu>
2018-07-09 17:49:28 +0200

    Fixed Code Coverage transformation

---
+++ scripts/upload_vsts_build_findings_webgoat.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+export JAVA_HOME=/e/bin/java8/
+#export HTTP_PROXY=http://httpproxy.munich.munichre.com:3128
+
+./gradlew webgoat:vstsUploadBuildFindings
+
+# Set the cache back just be before the last build
+sed -i 's/67363/67362/g' projects/webgoat_vsts_cache.json
